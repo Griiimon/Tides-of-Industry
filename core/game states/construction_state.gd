b89c54f99ac8dfd20079a7214ab5c9d5df0e7bf3
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-22 18:54:42 +0100

    Update construction_state.gd

--- core/game states/construction_state.gd
+++ core/game states/construction_state.gd
@@ -3,6 +3,7 @@ extends GameStateMachineState
 
 @export var building: Building
 
+
 var is_valid_position: bool= false:
 	set(b):
 		if not is_current_state(): return
@@ -14,8 +15,10 @@ var current_tile: Vector2i:
 		if not is_current_state(): return
 		current_tile= tile
 		var world: World= state_machine.world
-		is_valid_position= building.evaluate_placement_conditions(tile, world, world.get_island(tile)) 
-		prints("Is valid pos", is_valid_position)
+		if world.has_building(tile):
+			is_valid_position= false
+		else:
+			is_valid_position= building.evaluate_placement_conditions(tile, world, world.get_island(tile)) 
 
 
 
@@ -30,15 +33,21 @@ func on_exit():
 func on_unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		update_current_tile()
+	
 	elif event is InputEventMouseButton:
 		if event.is_pressed():
 			match event.button_index:
 				MOUSE_BUTTON_LEFT:
 					if is_valid_position:
 						build()
-						finished.emit()
+						if not event.shift_pressed:
+							finished.emit()
+						else:
+							update_current_tile(true)
+							
 				MOUSE_BUTTON_RIGHT:
 					finished.emit()
+					
 	elif event.is_action("build_mode") and event.is_pressed():
 		finished.emit()
 
@@ -56,8 +65,8 @@ func build():
 	#SignalManager.construction_error.emit("Can only build on settled islands")
 
 
-func update_current_tile():
+func update_current_tile(force_update: bool= false):
 	var new_tile: Vector2i= state_machine.world.get_mouse_tile()
-	if new_tile != current_tile:
+	if new_tile != current_tile or force_update:
 		current_tile= new_tile
 		state_machine.world.draw_ghost_building(current_tile, building)
