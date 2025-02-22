5355570a84c9a5ffafda546959d6fabd67ba08e8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-22 18:55:40 +0100

    Create ui_utils.gd

---
+++ core/utils/ui_utils.gd
@@ -0,0 +1,53 @@
+class_name UIUtils
+
+
+
+static func add_label(control: Control, text: String, horizontal= HORIZONTAL_ALIGNMENT_LEFT, min_size: Vector2= Vector2.ZERO)-> Label:
+	var label= Label.new()
+	label.text= text
+	label.horizontal_alignment= horizontal
+	label.custom_minimum_size= min_size
+	control.add_child(label)
+	return label
+
+
+static func add_empty(control: Control):
+	var label= Label.new()
+	control.add_child(label)
+	return label
+
+
+static func add_texture(control: Control, texture: Texture2D):
+	var texture_rect:= TextureRect.new()
+	texture_rect.texture= texture
+	control.add_child(texture_rect)
+	return texture_rect
+
+
+static func add_button(control: Control, text: String, on_pressed: Callable, args: Array= [])-> Button:
+	var button= Button.new()
+	button.text= text
+	if args.is_empty():
+		button.pressed.connect(on_pressed)
+	else:
+		button.pressed.connect(on_pressed.bind(args))
+
+	control.add_child(button)
+	return button
+
+
+static func add_timer(node: Node, time: float, one_shot: bool= true, auto_start: bool= false, callback= null)-> Timer:
+	var timer= Timer.new()
+	timer.wait_time= time
+	timer.one_shot= one_shot
+	timer.autostart= auto_start
+	if callback:
+		timer.timeout.connect(callback)
+	node.add_child(timer)
+	return timer
+
+
+static func free_children(node: Node):
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()
