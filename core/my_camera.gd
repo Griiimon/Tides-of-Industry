a25bba3f6964f8a1ed5cd4a25c63f7ead713ba6f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-28 09:35:46 +0100

    implement zooming

--- core/my_camera.gd
+++ core/my_camera.gd
@@ -1,7 +1,22 @@
 class_name MyCamera
 extends Camera2D
 
+enum ZoomState { DEFAULT, IN, OUT }
+
 @export var scroll_speed: float= 250
+
+var zoom_state: ZoomState= ZoomState.DEFAULT:
+	set(z):
+		if not is_inside_tree(): return
+		zoom_state= z
+		match zoom_state:
+			ZoomState.DEFAULT:
+				zoom= Vector2.ONE
+			ZoomState.IN:
+				zoom= Vector2(2, 2)
+			ZoomState.OUT:
+				zoom= Vector2(0.25, 0.25)
+		
 
 
 var previous_position: Vector2
@@ -17,10 +32,24 @@ func _process(delta: float) -> void:
 		previous_position= position
 		SignalManager.camera_moved.emit()
 
+	if Input.is_action_just_pressed("zoom_in"):
+		match zoom_state:
+			ZoomState.OUT:
+				zoom_state= ZoomState.DEFAULT
+			ZoomState.DEFAULT:
+				zoom_state= ZoomState.IN
+
+	elif Input.is_action_just_pressed("zoom_out"):
+		match zoom_state:
+			ZoomState.IN:
+				zoom_state= ZoomState.DEFAULT
+			ZoomState.DEFAULT:
+				zoom_state= ZoomState.OUT
+
 
 func scroll(vec: Vector2, delta: float, speed_factor: float= 1.0):
 	position_smoothing_enabled= false
-	position+= vec * delta * scroll_speed * speed_factor
+	position+= vec * delta * scroll_speed * speed_factor * 1.0 / zoom.x
 
 
 func scroll_to(target_pos: Vector2, smooth: bool= true):
